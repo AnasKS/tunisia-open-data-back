8d14c158c68e214b4e2629373ba546f776d6159e
Anas Ksibi <anas.ksibi@mobile-devices.fr>
2021-03-15 20:06:45 +0100

    [controllers]: change application controller inheritance.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-class ApplicationController < JSONAPI::ResourceController
+class ApplicationController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
 end
