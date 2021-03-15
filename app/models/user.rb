84cc0b57092c4c3f3e3c4373f9d65b3810a5d395
Anas Ksibi <anas.ksibi@mobile-devices.fr>
2021-03-15 22:18:16 +0100

    [models]: add validation & intials to use model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  after_initialize :set_defaults
+  
+  ROLES = [
+    USER = 'user',
+    ADMIN = 'admin',
+  ].freeze
+
+  # validations
+  validates :email, presence: true, uniqueness: true
+  validates_inclusion_of :role, in: ROLES
+
+  def admin?
+    role == ADMIN
+  end
+
+  def set_defaults
+    self.role = USER if role.nil?
+  end
 end
