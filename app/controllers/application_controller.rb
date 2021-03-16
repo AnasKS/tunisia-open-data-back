a7af523b491af4b97c0cafe74e89de957ee76107
Anas Ksibi <anas.ksibi@mobile-devices.fr>
2021-03-16 03:40:25 +0100

    [controllers]: allow cross site calls.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@
 
 class ApplicationController < ActionController::Base
   include JSONAPI::ActsAsResourceController
+  
+  protect_from_forgery with: :null_session, if: proc { |c| c.request.format =~ %r{application/json} }
 end
