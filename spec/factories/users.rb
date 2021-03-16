1c81f00b20b0b40092c12ac8984a78c50a15b262
Anas Ksibi <anas.ksibi@mobile-devices.fr>
2021-03-16 02:36:39 +0100

    [spec]: update user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
+  sequence :email do |n|
+    "fame-email-#{n + 900_000}@test.com"
+  end
   factory :user do
-    email { "MyString" }
-    role { "MyString" }
+    transient do
+      email { generate(:email) }
+    end
+    initialize_with do
+      args = attributes.merge(email: email)
+      User.new(args)
+    end
   end
 end
