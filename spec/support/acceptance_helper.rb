4639989dfd66df36f46871e5431fa923e585a7b5
Anas Ksibi <anas.ksibi@mobile-devices.fr>
2021-03-16 02:44:26 +0100

    [spec]: update output file for documentation.

--- spec/support/acceptance_helper.rb
+++ spec/support/acceptance_helper.rb
@@ -5,7 +5,7 @@ require 'rspec_api_documentation'
 require 'rspec_api_documentation/dsl'
 
 RspecApiDocumentation.configure do |config|
-  config.docs_dir = Rails.root.join('doc')
+  config.docs_dir = Rails.root.join('doc','generated')
   config.api_name = 'Tunisia Open Data API'
   config.keep_source_order = true
   config.format = [:api_blueprint]
