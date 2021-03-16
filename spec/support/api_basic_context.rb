ff7cbdf261f2b41c19f6e4fbc8a7a184a8c02905
Anas Ksibi <anas.ksibi@mobile-devices.fr>
2021-03-16 02:35:52 +0100

    [spec]: add shared context for auth support file.

---
+++ spec/support/api_basic_context.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'api basic context', shared_generators: :metadata do
+  let(:user) { create :user }
+  let(:admin) { create :user, role: User::ADMIN }
+
+  def generate_token user
+    payload = {
+      user: {
+        email: user.email
+      }
+    }
+
+    JWT.encode payload, '', 'HS256'
+  end
+end
